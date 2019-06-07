9bfa8867c31cbe997d4d29766217dd04164a3eae
Victor Llamas Olivares <victorllamas@gmail.com>
2019-06-06 20:48:13 -0500

    server.js file updated with functions get, post, put and delete

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get('/', function(rec, res){
-    res.send("Hello world!");
+app.get('/', function(req, res){
+    res.send(`Contestando con una peticion GET`);
 });
 
 app.listen(port, function(){
     console.log(`Example app listening port ${port}!`);
+});
+
+app.post('/', function(req, res){
+    res.send(`Contestando con una peticion POST`);
+})
+
+app.put('/', function(req,res){
+    res.send(`Constestando con una peticion PUT`);
+});
+
+app.delete('/', function(req, res){
+    res.send(`Contestando con una peticion DELETE`)
 });
